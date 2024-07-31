bbfd2b192bb33564ecf921fff1f61f5e4024074b
vzhang88 <vincent.zhang8989@gmail.com>
2024-08-01 08:36:39 +1000

    Phonebook MVP
    - Adding names
    - Warning for duplicates
    - Adding numbers
    - Search filter

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,34 +1,96 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  // state variable representing collection of people
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
+  // state variable representing current input name
+  const [newName, setNewName] = useState('')
+
+  // state variable representing current input number 
+  const [newNumber, setNewNumber] = useState('')
+
+  // state variable representing filter input search name
+  const [newSearchName, setNewSearchName] = useState('')
+
+  // handler for changes in input name 
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+    // console.log(newName)
+  }
+
+  // handler for changes in input number
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+    // console.log(newNumber)
+  }
+
+  // handler for changes in input search name
+  // note: every time we type something into the filter, we re-render the page
+  // and personsToShow is updated and displayed
+  const handleSearchNameChange = (event) => {
+    setNewSearchName(event.target.value)
+    // console.log(newSearchName)
+  }
+
+  const personsToShow = persons.filter(person=>person.name.toLowerCase().includes(newSearchName.toLowerCase()))
+  // console.log(personsToShow)
+
+  // handler for adding a person (form submission)
+  const addPerson = (event) => {
+
+    // prevent default action
+    event.preventDefault()
+
+    // create new name object
+    const person = {
+      name: newName, 
+      number: newNumber
+    }
+
+    const names = persons.map(person=>person.name)
+    if(names.includes(newName)){
+      alert(`${newName} is already added to phonebook`) // not '' but ``
+      setNewName('')
+      setNewNumber('')
+    }else{
+       // add new object to state variable representing collection of persons
+      setPersons(persons.concat(person))
+
+      // Clear once you have submitted - forces input to be cleared too
+      setNewName('')
+      setNewNumber('')
+    }
+  }
 
   return (
-    <>
+    <div>
+      <h2>Phonebook</h2>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        filter shown with <input value={newSearchName} onChange={handleSearchNameChange}/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <h2>add a new</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+
+      <h2>Numbers</h2>
+      {personsToShow.map(person=><div key={person.name}>{person.name} {person.number}</div>)}
+    </div>
   )
 }
 
