0d01d83e579277792cdd0c4f73cd38d98b880bf5
vzhang88 <vincent.zhang8989@gmail.com>
2024-08-01 12:33:44 +1000

    Added read from server to phonebook

---
+++ part2/notes/src/components/Note.jsx
@@ -0,0 +1,17 @@
+const Note = ({note}) => {
+    return (
+      <li>
+        {note.content}
+      </li>
+    )
+}
+
+const Paper = () => {
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default Note
--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,17 +1,13 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import People from './components/People'
 import PersonForm from './components/PersonForm'
+import axios from 'axios'
 
 const App = () => {
 
   // state variable representing collection of people
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
 
   // state variable representing current input name
   const [newName, setNewName] = useState('')
@@ -21,6 +17,18 @@ const App = () => {
 
   // state variable representing filter input search name
   const [newSearchName, setNewSearchName] = useState('')
+
+  // Retrieve data from server 
+  useEffect(() => {
+    console.log('effect')
+    axios
+      // not https
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  },[])
 
   // handler for changes in input name 
   const handleNameChange = (event) => {
@@ -78,7 +86,6 @@ const App = () => {
       <Filter newSearchName={newSearchName} handleSearchNameChange={handleSearchNameChange}/>
       <h2>add a new</h2>
       <PersonForm newName={newName} newNumber={newNumber} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} addPerson={addPerson}/>
-       
       <h2>Numbers</h2>
       <People personsToShow={personsToShow}/>
     </div>
