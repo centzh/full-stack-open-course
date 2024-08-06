bc5c3427b0d9b63faf06ca223ea19b1df451cb5c
vzhang88 <vincent.zhang8989@gmail.com>
2024-08-06 21:15:06 +1000

    Working form submission
    - Fixed form submission so that it works when input is entered in both input elements

--- projects/tracker/src/components/Display.jsx
+++ projects/tracker/src/components/Display.jsx
@@ -1,32 +1,64 @@
 import { useState } from "react"
 
-const handleInputChange = (event, setInputFunction) => {
-    const current_input = event.target.value
-    console.log(current_input)
-    setInputFunction(current_input)
-}
-
 const Display = () => {
-
+    
     const [accomplishInput, setAccomplishInput] = useState("")
     const [hoursInput, setHoursInput] = useState("")
+    const [dailyRecords, setDailyRecords] = useState([])
       
+    // event handler for changes in input elements
+    const handleInputChange = (event, setInputFunction) => {
+        const current_input = event.target.value
+        // console.log(current_input)
+        setInputFunction(current_input)
+    }
+
+    // event handler for form submissions
+    const handleFormSubmit = (event) => {
+        event.preventDefault()
+
+        // We can just define an object here, there is no template for an object
+        const new_record = {
+            accomplished: accomplishInput, 
+            hoursWorked: hoursInput,
+            id: dailyRecords.length // define id as record number so we can use as key, 
+        }
+    
+        setDailyRecords(dailyRecords.concat(new_record))
+        console.log(dailyRecords)
+    }
+ 
+
     return (
         <div>
             <h1>Daily tracker</h1>
         
-            <form>
+            <form onSubmit={handleFormSubmit}>
                 <p>What did you accomplish? </p> 
                 <input 
                     value={accomplishInput}
                     onChange={(event) => handleInputChange(event, setAccomplishInput)}
                 />
+            </form>
+
+            <form onSubmit={handleFormSubmit}>
                 <p>How many hours did you work?</p>
                 <input
                     value={hoursInput}
                     onChange={(event) => handleInputChange(event, setHoursInput)}
                 />
+                <button type="submit">save</button>
             </form>
+            <p></p>
+            
+            <div>
+                {dailyRecords.map(record=>
+                    <div key={record.id}>
+                        {record.accomplished} {record.hoursWorked}
+                    </div>
+                )}
+            </div>
+
         </div>
     )
 }
