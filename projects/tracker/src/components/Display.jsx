5dab564aa2602c93d276bdb174168559686fa998
vzhang88 <vincent.zhang8989@gmail.com>
2024-08-06 19:15:12 +1000

    Added second input
    - Added second input for hours worked

--- projects/tracker/src/components/Display.jsx
+++ projects/tracker/src/components/Display.jsx
@@ -1,23 +1,30 @@
 import { useState } from "react"
 
+const handleInputChange = (event, setInputFunction) => {
+    const current_input = event.target.value
+    console.log(current_input)
+    setInputFunction(current_input)
+}
+
 const Display = () => {
 
-    const [input, setInput] = useState("")
-
-    const handleInputChange = (event) => {
-        const current_input = event.target.value
-        console.log(current_input)
-        setInput(current_input)
-    }
-
+    const [accomplishInput, setAccomplishInput] = useState("")
+    const [hoursInput, setHoursInput] = useState("")
+      
     return (
         <div>
             <h1>Daily tracker</h1>
+        
             <form>
-                <p>What did you accomplish </p> 
+                <p>What did you accomplish? </p> 
                 <input 
-                    value={input}
-                    onChange={handleInputChange}
+                    value={accomplishInput}
+                    onChange={(event) => handleInputChange(event, setAccomplishInput)}
+                />
+                <p>How many hours did you work?</p>
+                <input
+                    value={hoursInput}
+                    onChange={(event) => handleInputChange(event, setHoursInput)}
                 />
             </form>
         </div>
